27e09241f5de7a943bab05828660b96088c62c3c
Weronika <wnaziemiec@gmail.com>
2019-05-17 21:15:03 +0200

    points from controller on the map

--- src/main/java/map/MapController.java
+++ src/main/java/map/MapController.java
@@ -3,14 +3,19 @@ package map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
 
 @Controller
 public class MapController {
 
     @GetMapping("/map")
-    public String map(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String map(Model model) {
+        ArrayList<Point2D.Double> points = new ArrayList<>(2);
+        points.add(new Point2D.Double(50.1,20.2));
+        points.add(new Point2D.Double(49.90,20.2));
+        model.addAttribute("points", points);
         return "map";
     }
 
