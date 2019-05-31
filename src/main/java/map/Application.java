50c0626ff770e4bb8f2d5fa3c85fd2d53d5c4200
Weronika <wnaziemiec@gmail.com>
2019-05-31 13:20:29 +0200

    GeoSpark and grid from file

--- src/main/java/map/Application.java
+++ src/main/java/map/Application.java
@@ -1,13 +1,45 @@
 package map;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.serializer.KryoSerializer;
+import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static java.lang.Math.cos;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    public static JavaSparkContext sc;
 
+    public static void main(String[] args) throws IOException {
+
+        SpringApplication.run(Application.class, args);
+
+        SparkConf conf = new SparkConf().setAppName("DynamicMap")
+            .setMaster("local[*]")
+            .set("spark.serializer", KryoSerializer.class.getName())
+            .set("spark.kryo.registrator", GeoSparkKryoRegistrator.class.getName());
+        sc = new JavaSparkContext(conf);
+
+
+        //TODO:zrobic pisanie przez geosparka
+        //TODO:spytac jak obliczyc 250m?
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("grid.csv"));
+//
+//        //https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
+//        for(Double lat = 50.009808; lat<50.052836; lat+= 0.00224578){
+//            Double lat_radian = lat * 3.14/180;
+//            for(Double lon = 19.895381; lon < 19.957671; lon+=0.00224579/cos(lat_radian)) {
+//                writer.write(lat.toString() + "," + lon.toString() +",3\n");
+//            }
+//        }
+//        writer.close();
+    }
 }
--- src/main/java/map/MapController.java
+++ src/main/java/map/MapController.java
@@ -1,22 +1,35 @@
 package map;
 
+import com.vividsolutions.jts.geom.Point;
+import org.datasyslab.geospark.enums.FileDataSplitter;
+import org.datasyslab.geospark.spatialRDD.PointRDD;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MapController {
 
     @GetMapping("/map")
     public String map(Model model) {
-        ArrayList<TimePoint> points = new ArrayList<>(2);
-        points.add(new TimePoint(13.6, new Point2D.Double(50.1,20.2)));
-        points.add(new TimePoint(4.8, new Point2D.Double(49.90,20.2)));
-        model.addAttribute("points", points);
+        String pointRDDInputLocation = "/home/weronika/magisterka/DynamicMap/grid.csv";
+        int pointRDDOffset = 0;
+        FileDataSplitter pointRDDSplitter = FileDataSplitter.CSV;
+        boolean carryOtherAttributes = true;
+        PointRDD objectRDD = new PointRDD(Application.sc, pointRDDInputLocation, pointRDDOffset, pointRDDSplitter, carryOtherAttributes);
+
+        Long pointsCount = objectRDD.rawSpatialRDD.count();
+        List<Point> pointList = objectRDD.rawSpatialRDD.take(pointsCount.intValue());
+        ArrayList<TimePoint> timePoints = new ArrayList<>();
+        pointList.stream().forEach(point -> timePoints.add(
+                new TimePoint(Double.parseDouble(point.getUserData().toString()),
+                                new Point2D.Double(point.getX(), point.getY()))
+                ));
+
+        model.addAttribute("points", timePoints);
         return "map";
     }
-
 }
