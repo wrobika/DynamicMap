6a3ef0468f19a68b9d1e204c496ef876bfca01d8
Weronika <wnaziemiec@gmail.com>
2020-06-02 12:37:05 +0200

    test return void

--- src/main/java/tests/TestController.java
+++ src/main/java/tests/TestController.java
@@ -4,12 +4,14 @@ import map.MapController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
 @Controller
 public class TestController {
     private MapController map = new MapController();
 
     @RequestMapping(value = "/testOneByOne")
+    @ResponseStatus(value = HttpStatus.OK)
     public void testOneByOne(@RequestParam String stringAmbulancePoints) {
         String[] points = stringAmbulancePoints.split(",");
         for(String point : points)
@@ -17,26 +19,31 @@ public class TestController {
     }
 
     @RequestMapping(value = "/testAll")
+    @ResponseStatus(value = HttpStatus.OK)
     public void testAll(@RequestParam String stringAmbulancePoints, @RequestParam int howManyTimes) {
+	String requestParam = "GEOMETRYCOLLECTION(" + stringAmbulancePoints + ")";
         for(int i = 0; i < howManyTimes; i++)
-            map.grid(stringAmbulancePoints);
+            map.grid(requestParam);
     }
 
     @RequestMapping(value = "/testIncrease")
+    @ResponseStatus(value = HttpStatus.OK)
     public void testIncrease(@RequestParam String stringAmbulancePoints) {
         String[] points = stringAmbulancePoints.split(",");
         for(int i = 0; i < points.length; i++) {
-            StringBuilder requestParams = new StringBuilder("GEOMETRYCOLLECTION(");
+            StringBuilder requestParam = new StringBuilder("GEOMETRYCOLLECTION(");
             for(int j = 0; j < i; j++){
-                requestParams.append(points[i]);
-                requestParams.append(",");
+                requestParam.append(points[j]);
+                requestParam.append(",");
             }
-            requestParams.append(")");
-            map.grid(requestParams.toString());
+	    requestParam.append(points[i]);
+            requestParam.append(")");
+            map.grid(requestParam.toString());
         }
     }
 
     @RequestMapping(value = "/testUpdate")
+    @ResponseStatus(value = HttpStatus.OK)
     public void testUpdate(@RequestParam String roadToUpdate, @RequestParam int howManyTimes) {
         for(int i = 0; i < howManyTimes; i++)
             map.update(roadToUpdate);
